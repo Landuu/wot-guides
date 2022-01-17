8af4244c3f5b17959e3d7942b91048866bed4caa
Bartłomiej Wiercigroch <landupcmr@gmail.com>
2022-01-17 17:57:48 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div className="app">
+      <div className="text-3xl text-center mt-20">Hello World</div>
+      <div className="text-xl text-center mt-2">~PogU</div>
+    </div>
+  );
+};
+
+export default App;
