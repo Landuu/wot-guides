447b2433aed9c8eb02f42f3a53c487c04a41c65e
Bartłomiej Wiercigroch <landupcmr@gmail.com>
2022-01-21 17:05:34 +0100

    Deploy api action test in prod

--- api/routes/tanks.js
+++ api/routes/tanks.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.send("Hello World! Test");
+    res.send("Hello World from prod!");
 })
 
 module.exports = router;
